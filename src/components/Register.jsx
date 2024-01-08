3ef8922f7ee7b4634317323ff066eb69b9b7904a
64070217 <88420101+64070217yongsinlimwilaikul@users.noreply.github.com>
2024-01-08 15:51:13 +0700

    oak

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,20 +1,17 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import { Alert, TextField, Button } from '@mui/material';
-import { useUserAuth } from '../context/UserAuthContext';
-import { fetchSignInMethodsForEmail } from 'firebase/auth';
+import React, { useState, useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEye, faEyeSlash } from '@fortawesome/free-regular-svg-icons';
-import styles from './Register.module.css';
-import { auth } from '../firebase';
+import { Link } from 'react-router-dom';
+import { Alert, TextField } from '@mui/material';
+import { useUserAuth } from '../context/UserAuthContext';
 
 const Register = () => {
-  const [step, setStep] = useState(1);
   const [passwordVisible, setPasswordVisible] = useState(false);
-  const [username, setUserName] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [error, setError] = useState('');
+
+  const [username, setUserName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
   const { signUp } = useUserAuth();
 
   const togglePasswordVisibility = () => {
@@ -23,104 +20,84 @@ const Register = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    setError('');
-
+    setError("");
     try {
-      if (step === 1) {
-        // Step 1: Check email
-        await signUp('', email, password);
-
-        // Move to step 2
-        setStep(2);
-      } else if (step === 2) {
-        // Step 2: Register with username
-        await signUp(username, email, password);
+      await signUp(username, email, password);
+    } catch (err) {
+      switch (err.code) {
+        case "auth/invalid-email":
+          setError("Invalid email address");
+          break;
+        case "auth/weak-password":
+          setError("Password should be at least 8 characters");
+          break;
+        case "auth/email-already-in-use":
+          setError("Email address is already in use");
+          break;
+        default:
+          setError("Something went wrong. Please try again.");
+          break;
       }
-    } catch (error) {
-      console.error('Error:', error.message);
-      setError('Something went wrong. Please try again.');
+      console.log(err);
     }
   };
 
   return (
     <div className="bg-gray-100 h-screen flex items-center justify-center">
+      <div className='text-2xl absolute top-2 left-2'>
+        <Link to="/">
+          return
+        </Link>
+      </div>
       <div className="bg-white p-8 rounded shadow-md w-96 mx-auto my-auto">
         <h1 className="text-2xl font-bold mb-6">Register</h1>
 
-        {error && (
-          <Alert severity="error" variant="filled" className="my-3">
-            {error}
-          </Alert>
-        )}
-
+        {error && <Alert severity="error" variant="filled" className='my-3'>{error}</Alert>}
         <form onSubmit={handleSubmit}>
-          {step === 1 && (
-            <div className="mb-4 relative">
-              <TextField
-                type="email"
-                label="Email"
-                variant="standard"
-                className="w-full"
-                onChange={(e) => setEmail(e.target.value)}
-              />
-              <div className="mb-4 relative">
-                <TextField
-                  type={passwordVisible ? 'text' : 'password'}
-                  label="Password"
-                  variant="standard"
-                  className="bg-white w-full"
-                  onChange={(e) => setPassword(e.target.value)}
-                />
-                <div
-                  className="absolute top-5 right-0 pr-2 flex items-center cursor-pointer"
-                  onClick={togglePasswordVisibility}
-                >
-                  <FontAwesomeIcon
-                    icon={passwordVisible ? faEye : faEyeSlash}
-                    id="eyeIcon"
-                    style={{ color: '#999999' }}
-                  />
-                </div>
-              </div>
-            </div>
-          )}
+          <div className="mb-4 relative">
+            <TextField type='text'
+              id="standard-basic"
+              label="Username"
+              variant="standard"
+              className='w-full'
+              onChange={(e) => setUserName(e.target.value)} />
+          </div>
+          <div className="mb-4 relative">
+            <TextField type='email'
+              id="standard-basic"
+              label="Email"
+              variant="standard"
+              className='w-full'
+              onChange={(e) => setEmail(e.target.value)} />
+          </div>
 
-          {step === 2 && (
-            <div className="mb-4 relative">
-              <TextField
-                type="text"
-                id="standard-basic"
-                label="Username"
-                variant="standard"
-                className="w-full"
-                onChange={(e) => setUserName(e.target.value)}
-              />
+          <div className="mb-4 relative">
+            <TextField type={passwordVisible ? 'text' : 'password'}
+              id="standard-basic"
+              label="Password"
+              variant="standard"
+              className='bg-white w-full'
+              onChange={(e) => setPassword(e.target.value)} />
+            <div
+              className="absolute top-5 right-0 pr-2 flex items-center cursor-pointer"
+              onClick={togglePasswordVisibility}
+            >
+              <FontAwesomeIcon icon={passwordVisible ? faEye : faEyeSlash} id="eyeIcon" style={{ color: "#999999" }} />
             </div>
-          )}
+          </div>
+
+          {/* Submit button */}
           <button
             type="submit"
             className="w-full bg-blue-500 text-white p-2 rounded hover:bg-blue-700 mb-2"
           >
-            {step === 1 ? 'Next' : 'Register'}
+            Register
           </button>
-        </form>
 
-        {step === 2 && (
-          <div>
-            <button
-              className="w-full bg-gray-500 text-white p-2 rounded hover:bg-gray-700 mb-2"
-              onClick={() => setStep(1)}
-            >
-              Go Back to Step 1
-            </button>
-            <p className="mt-4">
-              Already have an account?{' '}
-              <Link to="/Login" className="text-blue-500">
-                Log in here
-              </Link>
-            </p>
-          </div>
-        )}
+          <p className="mt-4">
+            Already have an account? <Link to="/Login" className="text-blue-500">Log in here</Link>
+          </p>
+        </form>
       </div>
     </div>
   );
