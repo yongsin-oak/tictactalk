8bb1b019f5e60e8157df66a34cc42391d0c254b1
yongsin-oak <64070217@it.kmitl.ac.th>
2024-02-16 23:32:13 +0700

    Update Chat.jsx

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,49 +1,49 @@
 import React, { useState, useEffect } from 'react';
 
-const Chat = ({ socket, roomCode, user }) => {
+const Chat = ({ roomCode, user }) => {
     const [messages, setMessages] = useState([]);
     const [newMessage, setNewMessage] = useState('');
     const [isTyping, setIsTyping] = useState(false);
     const [anotherPlayer, setAnotherPlayer] = useState('');
 
-    useEffect(() => {
-        // Listen for incoming messages from the server
-        socket.on('chatMessage', (message) => {
-            setMessages(message);
-            // setMessages((prevMessages) => [...prevMessages, message]);
-        });
+    // useEffect(() => {
+    //     // Listen for incoming messages from the server
+    //     socket.on('chatMessage', (message) => {
+    //         setMessages(message);
+    //         // setMessages((prevMessages) => [...prevMessages, message]);
+    //     });
 
-        // Clean up socket listener when component unmounts
-        return () => {
-            socket.off('chatMessage');
-        };
-    }, [socket]);
+    //     // Clean up socket listener when component unmounts
+    //     return () => {
+    //         socket.off('chatMessage');
+    //     };
+    // }, [socket]);
 
     const sendMessage = () => {
         if (newMessage.trim() !== '') {
-            socket.emit('sendMessage', { message: newMessage, roomCode: roomCode, displayName: user.displayName });
+            // socket.emit('sendMessage', { message: newMessage, roomCode: roomCode, displayName: user.displayName });
             setNewMessage('');
-            socket.emit('typing', {roomCode, displayName: user.displayName, isTyping: false });
+            // socket.emit('typing', {roomCode, displayName: user.displayName, isTyping: false });
         }
     };
     const handleChange = (e) => {
         setNewMessage(e.target.value);
-        socket.emit('typing', {roomCode, displayName: user.displayName, isTyping: true });
+        // socket.emit('typing', {roomCode, displayName: user.displayName, isTyping: true });
         if (e.target.value === ''){
-            socket.emit('typing', {roomCode, displayName: user.displayName, isTyping: false });
+            // socket.emit('typing', {roomCode, displayName: user.displayName, isTyping: false });
         }
         
         setTimeout(() => {
-            socket.emit('typing', {roomCode, displayName: user.displayName, isTyping: false });
+            // socket.emit('typing', {roomCode, displayName: user.displayName, isTyping: false });
         }, 7000);
     }
 
-    socket.on('typing', ({ roomCode: receivedRoomCode, isTyping, displayName }) => {
-        if (receivedRoomCode === roomCode) {
-            setAnotherPlayer(displayName);
-            setIsTyping(isTyping);
-        }
-    });
+    // socket.on('typing', ({ roomCode: receivedRoomCode, isTyping, displayName }) => {
+    //     if (receivedRoomCode === roomCode) {
+    //         setAnotherPlayer(displayName);
+    //         setIsTyping(isTyping);
+    //     }
+    // });
     return (
         <div className='text-start'>
             <div>
