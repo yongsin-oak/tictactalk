0ead06a70e09ba9e5d33ab5ebb7a84eb89442def
yongsin-oak <64070217@it.kmitl.ac.th>
2024-02-16 23:58:16 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -20,9 +20,7 @@ const router = createBrowserRouter([
   },
 ])
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <UserAuthContextProvider>
-      <RouterProvider router={router} />
-    </UserAuthContextProvider>
-  </React.StrictMode>
+  <UserAuthContextProvider>
+    <RouterProvider router={router} />
+  </UserAuthContextProvider>
 )
