843b56f56f042aa381e7c447860cf8318c8ec75f
yongsin-oak <64070217@it.kmitl.ac.th>
2024-02-19 12:28:50 +0700

    sad

--- client/src/components/tictactoe.jsx
+++ client/src/components/tictactoe.jsx
@@ -39,20 +39,20 @@ function Tictactoe() {
         if (socket) {
             return;
         }
-        // const newSocket = io('https://tictactalk.as.r.appspot.com/', {
-        //     transports: ['websocket'],
-        //     autoConnect: true,
-        //     cors: {
-        //         origin: '*',
-        //     },
-        // });
-        const newSocket = io('http://localhost:8080', {
+        const newSocket = io('https://tictactalk.as.r.appspot.com/', {
             transports: ['websocket'],
             autoConnect: true,
             cors: {
                 origin: '*',
             },
         });
+        // const newSocket = io('http://localhost:8080', {
+        //     transports: ['websocket'],
+        //     autoConnect: true,
+        //     cors: {
+        //         origin: '*',
+        //     },
+        // });
         setSocket(newSocket);
 
         return () => {
