f2699f5e31477d9d0ef930d2dcaa8cabf5077f38
yongsin-oak <64070217@it.kmitl.ac.th>
2024-03-10 00:33:03 +0700

    complete

--- client/src/components/HomepageLogging.jsx
+++ client/src/components/HomepageLogging.jsx
@@ -38,20 +38,20 @@ function HomepageLogging() {
         if (socket) {
             return;
         }
-        // const newSocket = io('https://tictactalk.as.r.appspot.com/', {
-        //     transports: ['websocket'],
-        //     autoConnect: true,
-        //     cors: {
-        //         origin: '*',
-        //     },
-        // });
-        const newSocket = io('http://localhost:8080', {
+        const newSocket = io('https://tictactalk.as.r.appspot.com/', {
             transports: ['websocket'],
             autoConnect: true,
             cors: {
                 origin: '*',
             },
         });
+        // const newSocket = io('http://localhost:8080', {
+        //     transports: ['websocket'],
+        //     autoConnect: true,
+        //     cors: {
+        //         origin: '*',
+        //     },
+        // });
         setSocket(newSocket);
 
         return () => {
